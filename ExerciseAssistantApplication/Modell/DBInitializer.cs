518451ac3520a6ff4a0fbc1594d547c5bf5bae99
nagyiza <32398768+nagyiza@users.noreply.github.com>
2017-09-30 19:19:36 +0300

    .

---
+++ ExerciseAssistantApplication/Modell/DBInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace ExerciseAssistantApplication.Modell
+{
+    public class DBInitializer : DropCreateDatabaseAlways<MyDbContext> //CreateDatabaseIfNotExists<TMCatalogDB>
+    //public class DBInitializer : CreateDatabaseIfNotExists<MyDbContext>
+    {
+        protected override void Seed(MyDbContext context)
+        {
+            context.Users.Add(new User { UserId = 0, Username = "iza", Password = "123", Email = "iza", IsAdmin = true, IsActive = true});
+            context.Users.Add(new User { UserId = 1, Username = "vivien", Password = "vivien", Email = "vivi", IsAdmin = false, IsActive = true});
+        }
+    }
+}
---
+++ ExerciseAssistantApplication/ViewModell/MainWindowViewModel.cs
@@ -0,0 +1,60 @@
+﻿using ExerciseAssistantApplication.Common;
+using ExerciseAssistantApplication.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseAssistantApplication.ViewModell
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public RelayCommand SignInCommand { get; set; }
+        public RelayCommand RegistrationCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            this.SignInCommand = new RelayCommand(SignInClick, SignInCancel);
+            this.RegistrationCommand = new RelayCommand(Registration, RegistrationCancel);
+
+            this.NewExercise = new RelayCommand(NewExerciseClick, NewExerciseCancel);
+        }
+
+        public void SignInClick()
+        {
+            //hide before startup
+            LogInViewModel blpvm = new LogInViewModel();
+            ViewService.ShowDialog(blpvm);
+        }
+        private bool SignInCancel()
+        {
+            return true;
+        }
+        private void Registration()
+        {
+            RegistrationViewModel rvm = new RegistrationViewModel();
+            ViewService.ShowDialog(rvm);
+        }
+        private bool RegistrationCancel()
+        {
+            return true;
+        }
+
+        public RelayCommand NewExercise { get; set; }
+        public void NewExerciseClick()
+        {
+            //NewExerciseViewModel nevm = new NewExerciseViewModel();
+            //ViewService.ShowDialog(nevm);
+
+            ReferenceDataCollection.MainWindow m = new ReferenceDataCollection.MainWindow();
+            m.Show();
+        }
+        public bool NewExerciseCancel()
+        {
+            return true;
+        }
+    }
+}
